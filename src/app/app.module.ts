887ee9bba95b29f6c9b3e4c4394c8c10bd9571f3
JohenTeng <1078481395>
2017-08-09 15:00:29 +0800

    动画，table表，圆形按钮插件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CircleLiveMenuButtonComponent } from '../../src/app/circle-live-menu-button/circle-live-menu-button.component';
+import { TableShowComponent } from '../../src/app/table-show/table-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleLiveMenuButtonComponent,
+    TableShowComponent
   ],
   imports: [
     BrowserModule
